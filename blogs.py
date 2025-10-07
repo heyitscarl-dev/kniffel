71f421bd7d03de78f7c4d15e9f6c250897e63b03
tobiasmalcherek11-wq <tobias.malcherek11@gmail.com>
2025-10-07 10:24:04 +0200

    wip: kniffel blogs

---
+++ blogs.py
@@ -0,0 +1,8 @@
+from gpanel import *
+makeGPanel(0,100,0,100)
+rectangle(10,20,50,90)
+rectangle(60,20,100,90)
+fnt = Font("Arial", 3, 20)
+text(11,89,"Kniffel", fnt, "blue", "white")
+text(61,89,"Kniffel", fnt, "blue", "white")
+text(61,83,"Kniffel")
