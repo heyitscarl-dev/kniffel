a1c4415869db547151840c9e5d7bf83db914fffd
heyitscarl <carl.nw.neben@gmail.com>
2025-10-18 00:01:12 +0200

    feat: dice drawing

--- kniffel/app.py
+++ kniffel/app.py
@@ -2,22 +2,13 @@ from enum import Enum, auto
 from kniffel import ui
 
 import pygame
+import random
 
 FRAMERATE = 60
 
-class Consequence(Enum):
-    CONTINUE = auto()
-    QUIT = auto()
-
-def update() -> Consequence:
-    for e in pygame.event.get():
-        if (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE
-        or e.type == pygame.QUIT):
-            return Consequence.QUIT
-
-    return Consequence.CONTINUE
-
 def main() -> None:
+    die = ui.Die(5)
+
     running = True
 
     interface = ui.Interface()
@@ -26,10 +17,20 @@ def main() -> None:
     while running:
         dt = clock.tick(FRAMERATE)
 
-        if update() == Consequence.QUIT:
-            running = False
+        for e in pygame.event.get():
+            if (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE
+            or e.type == pygame.QUIT):
+                running = False
+            elif e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
+                die.set(random.randint(1, 6))
 
         interface.prepare()
+
+        die.draw(interface, pygame.Vector2(
+            interface.dimensions.x // 2,
+            interface.dimensions.y // 2
+        ))
+
         interface.finish()
 
     pygame.quit()
--- kniffel/ui.py
+++ kniffel/ui.py
@@ -24,3 +24,74 @@ class Interface:
 
     def finish(self):
         pygame.display.flip()
+
+DIE_SIZE = 50
+DIE_BACKGROUND = (0, 0, 0)
+DIE_FOREGROUND = (255, 255, 255)
+DIE_PIP_SIZE = DIE_SIZE // 10
+
+LEFT = 0.2
+CENTER = 0.5
+RIGHT = 0.8
+
+TOP = 0.8           # glaube ich(?)
+BOTTOM = 0.2
+
+class Die:
+    value: int
+    kept: bool
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.kept = False
+
+    def set(self, value: int):
+        self.value = value
+
+    def toggle_keep(self):
+        self.keep = not self.keep
+
+    def draw(self, interface: Interface, center: pygame.Vector2):
+        surface = pygame.Surface((DIE_SIZE, DIE_SIZE))
+        pygame.draw.rect(surface, DIE_BACKGROUND, [0, 0, DIE_SIZE, DIE_SIZE])
+
+        def pip(relative_x: float, relative_y: float):
+            pygame.draw.circle(
+                surface,
+                DIE_FOREGROUND, 
+                (relative_x * DIE_SIZE, relative_y * DIE_SIZE), 
+                DIE_PIP_SIZE
+            )
+
+        if self.value == 1:
+            pip(CENTER, CENTER)
+        elif self.value == 2:
+            pip(LEFT, TOP)
+            pip(RIGHT, BOTTOM)
+        elif self.value == 3:
+            pip(LEFT, TOP)
+            pip(CENTER, CENTER)
+            pip(RIGHT, BOTTOM)
+        elif self.value == 4:
+            pip(LEFT, TOP)
+            pip(LEFT, BOTTOM)
+            pip(RIGHT, TOP)
+            pip(RIGHT, BOTTOM)
+        elif self.value == 5:
+            pip(LEFT, TOP)
+            pip(LEFT, BOTTOM)
+            pip(RIGHT, TOP)
+            pip(RIGHT, BOTTOM)
+            pip(CENTER, CENTER)
+        elif self.value == 6:
+            pip(LEFT, TOP)
+            pip(LEFT, CENTER)
+            pip(LEFT, BOTTOM)
+            pip(RIGHT, TOP)
+            pip(RIGHT, CENTER)
+            pip(RIGHT, BOTTOM)
+
+        interface.surface.blit(surface, (
+            center.x - DIE_SIZE // 2,
+            center.y - DIE_SIZE // 2
+        ))
